eaf6db1fa0b09e6713164fe5aefee0d955fef3f1
Jack T <jack@jackt.space>
2024-12-18 02:17:59 -0800

    d1p2

--- days/day_1/src/main.rs
+++ days/day_1/src/main.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+#[allow(dead_code)]
+static EXAMPLE: &str = include_str!("example.txt");
 static INPUT: &str = include_str!("input.txt");
 
 fn part_1() {
@@ -24,6 +28,32 @@ fn part_1() {
     println!("{total_dist}");
 }
 
+fn part_2() {
+    let columns: (Vec<u32>, Vec<u32>) = INPUT
+        .lines()
+        .filter_map(|line| {
+            let mut split = line.split_whitespace();
+            let first = split.next()?.parse::<u32>().ok()?;
+            let second = split.next()?.parse::<u32>().ok()?;
+
+            Some((first, second))
+        })
+        .collect();
+
+    let mut second_list_set = HashMap::new();
+    for n in columns.1 {
+        second_list_set.insert(n, second_list_set.get(&n).map(|c| c + 1).unwrap_or(1));
+    }
+
+    let mut score = 0;
+    for n in columns.0 {
+        score += second_list_set.get(&n).map(|c| c * n).unwrap_or(0);
+    }
+
+    println!("{score}")
+}
+
 fn main() {
     part_1();
+    part_2();
 }
