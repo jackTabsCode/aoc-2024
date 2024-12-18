095a2e9ddebe6cf09f75b0f8759fb38d34a70d33
Jack T <jack@jackt.space>
2024-12-18 02:44:41 -0800

    d2p2

--- days/day_2/src/main.rs
+++ days/day_2/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 #[allow(dead_code)]
 static EXAMPLE: &str = include_str!("example.txt");
 static INPUT: &str = include_str!("input.txt");
@@ -47,7 +45,71 @@ fn part_1() {
     println!("{reports_safe}")
 }
 
-fn part_2() {}
+fn part_2_report(levels: &[i32]) -> bool {
+    let mut peekable = levels.iter().peekable();
+
+    let mut sign: Option<i32> = None;
+
+    while let Some(level) = peekable.next() {
+        let next = peekable.peek();
+        if let Some(next) = next {
+            let diff = level - *next;
+            let diff_sign = diff.signum();
+
+            if let Some(sign) = sign {
+                if diff_sign != sign {
+                    return false;
+                }
+            } else {
+                sign = Some(diff_sign);
+            }
+
+            if diff.abs() > 3 || diff.abs() < 1 {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
+fn part_2() {
+    let mut reports_safe = 0;
+
+    for line in INPUT.lines() {
+        let levels: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse::<i32>().ok())
+            .collect();
+
+        if part_2_report(&levels) {
+            reports_safe += 1;
+            continue;
+        }
+
+        let mut safe = false;
+
+        for (index, _) in levels.iter().enumerate() {
+            if safe {
+                break;
+            }
+
+            let mut new = levels.clone();
+            new.remove(index);
+
+            if part_2_report(&new) {
+                safe = true;
+                break;
+            }
+        }
+
+        if safe {
+            reports_safe += 1;
+        }
+    }
+
+    println!("{reports_safe}")
+}
 
 fn main() {
     part_1();
